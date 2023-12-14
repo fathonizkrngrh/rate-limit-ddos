b9d1340b015d1376e546f3bfadbcbb9232737002
fathoni <fathonizikri11@gmail.com>
2023-12-14 12:25:34 +0700

    --

--- server/src/middlewares/limiter.middleware.js
+++ server/src/middlewares/limiter.middleware.js
@@ -5,13 +5,13 @@ const { apiTooManyRequestResponse } = require("../utils/apiResponse.utils");
 module.exports = {
     limiter: rateLimit({
         windowMs: 3 * 60 * 1000,
-        max: 1000,
+        max: 100,
         handler: (req, res, next) => {
             return res.status(status.TOO_MANY_REQUESTS).json(apiTooManyRequestResponse('Too many requests, please try again later.'));
         }
     }),
     loginLimit: rateLimit({
-        windowMs: 1 * 60 * 1000,
+        windowMs: 3 * 60 * 1000,
         max: 3,
         handler: (req, res, next) => {
             return res.status(status.TOO_MANY_REQUESTS).json(apiTooManyRequestResponse('Too many requests from this IP, please try again after 3 minutes'));
